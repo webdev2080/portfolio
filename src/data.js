88e26c81c632e2bf9cfe5fb9ec4459611535c833
Ryan Helferich <webdev2080@gmail.com>
2022-05-02 23:51:23 -0500

    Built Projects.js page

---
+++ src/data.js
@@ -0,0 +1,18 @@
+// src/data.js
+
+export const projects = [
+    {
+        title: "Project 1",
+        subtitle: "Project-Subtitle",
+        description: "Description",
+        image: "",
+        link: "./sqlcheatsheet.png"
+    },
+    {
+        title: "Project 2",
+        subtitle: "Project-Subtitle",
+        description: "Description",
+        image: "",
+        link: ""
+    }
+];
