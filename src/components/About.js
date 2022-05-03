2fda355dd09a1105730166f98f5fa767876ae33d
Ryan Helferich <webdev2080@gmail.com>
2022-05-03 01:00:20 -0500

    Made text blue Lol

--- src/components/About.js
+++ src/components/About.js
@@ -16,7 +16,7 @@ export default function About(){
                         Some stuff about me should go here, I'll add it later!
                     </p>
                     <div className="flex justify-center">
-                        <a href="#contact" className="inline-flex text-white bg-blue-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg">
+                        <a href="#contact" className="inline-flex text-white bg-blue-500 border-0 py-2 px-6 focus:outline-none hover:bg-blue-600 rounded text-lg">
                             Work With Me
                         </a>
                         <a href="#projects" className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg">
