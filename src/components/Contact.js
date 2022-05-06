525cdc2479ecb3a642d9ac8b82cfb696fd91d5bc
Ryan Helferich <webdev2080@gmail.com>
2022-05-05 23:54:47 -0500

    Fixed Href email issue

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -56,9 +56,9 @@ export default function Contact() {
                 <h2 className="title-font font-semibold text-white tracking-widest text-xs">
                   EMAIL
                 </h2>
-                <a className="text-indigo-400 leading-relaxed">
+                <div className="text-indigo-400 leading-relaxed">
                   rhelferich2080@gmail.com
-                </a>
+                </div>
                 <h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">
                   PHONE
                 </h2>
