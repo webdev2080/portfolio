e98c7cbec810e0df0fabde8b5f673ff242553fe0
Ryan Helferich <webdev2080@gmail.com>
2022-05-05 23:36:50 -0500

    Added Navbar

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -22,7 +22,7 @@ export default function Projects(){
                 className="sm:w-1/2 w-100 p-4">
                     <div className="flex relative">
                         <img alt="gallery" className="absolute inset-0 w-full h-full object-cover object-center" src={project.image}/>
-                        <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-800 bg-gray-900 opacity-90 hover:opacity-100">
+                        <div className="px-8 py-10 relative z-9 w-full border-4 border-gray-800 bg-gray-900 opacity-90 hover:opacity-100">
                             <h2 className="tracking-widest text-sm title-font font-medium text-blue-400 mb-1">
                                 {project.subtitle}
                             </h2>
