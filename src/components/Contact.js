dc380b9ea5177ba24522387de893f869a840814f
webdev2080 <webdev2080@gmail.com>
2023-05-02 22:21:47 -0500

    Updated Contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -62,7 +62,7 @@ export default function Contact() {
                 <h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">
                   PHONE
                 </h2>
-                <p className="leading-relaxed">(951)-760-0184</p>
+                <p className="leading-relaxed">(737)-248-6536</p>
               </div>
             </div>
           </div>
